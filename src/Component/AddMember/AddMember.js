98c98affe75a475d3b10c3f242519aa0bd0b07bf
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-13 18:25:14 +0530

    Changed UI

--- src/Component/AddMember/AddMember.js
+++ src/Component/AddMember/AddMember.js
@@ -1,15 +1,17 @@
 import './AddMember.css';
+import Card from '../Card/Card';
 
 const AddMember = () => {
     return (
-        <div>
+        <Card>
             <form className="form_input">
                 <label>Enter Your Name</label>
                 <input type='text' placeholder='Eg. Prabhash Rai'></input>
                 <label>Enter Your Age</label>
                 <input type='number' placeholder='Eg. 21'></input>
+                <button type='submit'>Add User</button>
             </form>
-        </div>
+        </Card>
     )
 }
 
