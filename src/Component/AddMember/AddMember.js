ddc973e5b19df57400315ce2942bc3832a2ce04d
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-13 18:16:36 +0530

    Added form to take input

--- src/Component/AddMember/AddMember.js
+++ src/Component/AddMember/AddMember.js
@@ -3,6 +3,12 @@ import './AddMember.css';
 const AddMember = () => {
     return (
         <div>
+            <form className="form_input">
+                <label>Enter Your Name</label>
+                <input type='text' placeholder='Eg. Prabhash Rai'></input>
+                <label>Enter Your Age</label>
+                <input type='number' placeholder='Eg. 21'></input>
+            </form>
         </div>
     )
 }
