0daf9e66a0cf0b47796d74f2636c5085bf8d2778
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-17 11:56:33 +0530

    used protal to categorise components

--- src/Component/ErrorModel/ErrorModel.js
+++ src/Component/ErrorModel/ErrorModel.js
@@ -2,24 +2,45 @@ import Button from '../Button/Button';
 import Card from '../Card/Card';
 import './ErrorModel.css';
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Backdrop = (props) => {
+    return <div className='backdrop'/>
+};
+
+const Overlay = (props) => {
+    return (
+        <Card className='modal'>
+            <header className='header'>
+                <h2>{props.head}</h2>
+            </header>
+            <div className='content'>
+                <p>{props.message}</p>
+            </div>
+            <footer className='actions'>
+                <Button onClick={props.onClick}>
+                    Okay
+                </Button>
+            </footer>
+        </Card>
+    );
+};
 
 const ErrorModel = (props) => {
     return(
         <React.Fragment>
-            <div className='backdrop' />
-            <Card className='modal'>
-                <header className='header'>
-                    <h2>{props.head}</h2>
-                </header>
-                <div className='content'>
-                    <p>{props.message}</p>
-                </div>
-                <footer className='actions'>
-                    <Button onClick={props.onClick}>
-                        Okay
-                    </Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+            <Backdrop
+            />, document.getElementById('backdrop-root')
+            )}
+
+            {ReactDOM.createPortal(
+            <Overlay 
+                head= {props.head}
+                message= {props.message}
+                onClick= {props.onClick}
+            />, document.getElementById('overlay-root')
+            )}
         </React.Fragment>
     )
 }
